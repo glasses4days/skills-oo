dd9b1dbbad42beb2dbda0848390d5b4f14cc74ab
glasses4days <cooper@Coopers-MacBook-Air.local>
2016-04-17 19:28:17 -0700

    Stuck on part 3

--- skills-oo.py
+++ skills-oo.py
@@ -1,6 +1,6 @@
 class Student(object):
 
-    def __init__(first_name, last_name, address):
+    def __init__(self, first_name, last_name, address):
         """Initialize student attribute"""
 
         self.first_name = first_name
@@ -10,15 +10,23 @@ class Student(object):
 
 class Question(object):
 
-    def __init__(question, correct_answer):
+    def __init__(self, question, correct_answer):
         """Initialize question and correct answer"""
 
         self.question = question
         self.correct_answer = correct_answer
+        q_and_a = {}
+        q_and_a[self.question] = self.correct_answer
+
 
 class Exam(Question):
 
-    def __init__(name):
+    def __init__(self, name):
 
         self.name = name
         self.questions = []
+
+    def add_question(self, question, correct_answer):
+       self.questions.append(super(Exam, self).__init__(question, correct_answer))
+       #I'm trying add the question and answer using the Question object. It won't
+       #append and I'm not sure why.
